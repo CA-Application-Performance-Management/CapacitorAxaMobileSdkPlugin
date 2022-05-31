a7d430bc555281f213a134251783d4a4f37ed003
ArunaYarra <aruna.yarra@broadcom.com>
2022-05-31 17:18:35 +0530

    Added iOS Support for all the Custom APIs

--- ios/Plugin/CapacitorAxaMobileSdkPlugin.m
+++ ios/Plugin/CapacitorAxaMobileSdkPlugin.m
@@ -5,10 +5,36 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorAxaMobileSdkPlugin, "CapacitorAxaMobileSdk",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(setCustomerId, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(getCustomerId, CAPPluginReturnPromise);
+           
            CAP_PLUGIN_METHOD(disableSDK, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(enableSDK, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(isSDKEnabled, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(getDeviceId, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getCustomerId, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setCustomerId, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setSessionAttribute, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(enterPrivateZone, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(exitPrivateZone, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isInPrivateZone, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getAPMHeader, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(addToAPMHeader, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setSSLPinningMode, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stopCurrentSession, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startNewSession, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stopCurrentAndStartNewSession, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startApplicationTransaction, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stopApplicationTransaction, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setCustomerFeedback, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setCustomerLocation, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(sendScreenShot, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(viewLoaded, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(ignoreView, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(ignoreViews, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isScreenshotPolicyEnabled, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(logNetworkEvent, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(logTextMetric, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(logNumericMetric, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(uploadEvents, CAPPluginReturnPromise);
+
+
 )
--- ios/Plugin/CapacitorAxaMobileSdkPlugin.swift
+++ ios/Plugin/CapacitorAxaMobileSdkPlugin.swift
@@ -20,10 +20,7 @@ public class CapacitorAxaMobileSdkPlugin: CAPPlugin {
      * This function is internal and only for passing NSError * items to JS as a string
      */
     func CAMAAErrorString(error: NSError) -> String {
-        if (error != nil) {
-            return String(format: "%@: %ld %@", error.domain, error.code, error.userInfo["NSLocalizedDescription"] as! CVarArg)
-        }
-        return String()
+        return String(format: "%@: %ld %@", error.domain, error.code, error.userInfo["NSLocalizedDescription"] as! CVarArg)
     }
     
     @objc func disableSDK(_ call: CAPPluginCall) {
@@ -39,30 +36,29 @@ public class CapacitorAxaMobileSdkPlugin: CAPPlugin {
     @objc func isSDKEnabled(_ call: CAPPluginCall) {
         let isEnabled = CAMDOReporter.isSDKEnabled()
         call.resolve([
-            "Enabled": isEnabled
+            "value": isEnabled
         ])
     }
     
     @objc func getDeviceId(_ call: CAPPluginCall) {
         let deviceId = CAMDOReporter.deviceId()
         call.resolve([
-            "deviceId": deviceId!
+            "value": deviceId ?? ""
+        ])
+    }
+
+    @objc func getCustomerId(_ call: CAPPluginCall) {
+        let customerId = CAMDOReporter.customerId()
+        call.resolve([
+            "value": customerId as Any
         ])
     }
     
-    /**
-     * Use this API to set the customer ID for this session.
-     *
-     * @param customerId is a string containing the customer ID
-     * @param callback is a function which expects an (SDKError value)
-     *
-     * If an empty string is passed, the customer iD is reset.
-     *
-     */
-    
     @objc func setCustomerId(_ call: CAPPluginCall) {
         guard let customerId = call.getString("customerId"), !customerId.isEmpty else {
-            call.reject("Customer Id is nil")
+            call.resolve([
+                "error": "No Customer Id"
+            ])
             return
         }
         let error: SDKError = CAMDOReporter.setCustomerId(customerId)
@@ -70,29 +66,320 @@ public class CapacitorAxaMobileSdkPlugin: CAPPlugin {
             "error": error
         ])
     }
-
-    /**
-     * Use this API to get the customer ID for this session.
-     * @param callback is a function which expects an string value
-     *
-     * If the customer ID is not set, this API returns a null value.
-     *
-     */
-    @objc func getCustomerId(_ call: CAPPluginCall) -> String? {
-        let response = CAMDOReporter.customerId()
-        return response
+    
+    @objc func setSessionAttribute(_ call: CAPPluginCall) {
+        guard let name = call.getString("name"), !name.isEmpty else {
+            call.resolve([
+                "error": "No session attribute name"
+            ])
+            return
+        }
+        
+        guard let value = call.getString("value"), !name.isEmpty else {
+            call.resolve([
+                "error": "No session attribute value"
+            ])
+            return
+        }
+        
+        let error: SDKError = CAMDOReporter.setSessionAttribute(name, withValue: value)
+        call.resolve([
+            "error": error
+        ])
     }
     
-    /**
-     * Use this API to determine if the SDK is enabled or not.
-     *
-     * @param callback is a function which expects a boolean value
-     *
-    */
-//    @objc func isSDKEnabled(_ call: CAPPluginCall) -> Bool {
-//        let response = CAMDOReporter.isSDKEnabled()
-//        return response
-//    }
+    @objc func enterPrivateZone(_ call: CAPPluginCall) {
+        CAMDOReporter.enterPrivateZone()
+        call.resolve()
+    }
     
+    @objc func exitPrivateZone(_ call: CAPPluginCall) {
+        CAMDOReporter.exitPrivateZone()
+        call.resolve()
+    }
+    
+    @objc func isInPrivateZone(_ call: CAPPluginCall) {
+        let isInPrivate = CAMDOReporter.isInPrivateZone()
+        call.resolve([
+            "value": isInPrivate
+        ])
+    }
+    
+    @objc func getAPMHeader(_ call: CAPPluginCall) {
+        let apmHeader = CAMDOReporter.apmHeader()
+        call.resolve([
+            "value": apmHeader as Any
+        ])
+    }
+    
+    @objc func addToAPMHeader(_ call: CAPPluginCall) {
+        guard let apmHeader = call.getString("data"), !apmHeader.isEmpty else {
+            call.reject("No APM header")
+            return
+        }
+        CAMDOReporter.add(toApmHeader: apmHeader)
+        call.resolve()
+    }
+    
+    @objc func setSSLPinningMode(_ call: CAPPluginCall) {
+        guard let pinningMode = call.getInt("pinningMode"), let pinnedValues = call.getArray("pinnedValues") else {
+            return
+        }
+        CAMDOReporter.setSSLPinningMode(CAMDOSSLPinningMode(rawValue: UInt(pinningMode))!, withValues: pinnedValues)
+        call.resolve()
+    }
+    
+    @objc func stopCurrentSession(_ call: CAPPluginCall) {
+        CAMDOReporter.stopCurrentSession()
+        call.resolve()
+    }
+    
+    @objc func startNewSession(_ call: CAPPluginCall) {
+        CAMDOReporter.startNewSession()
+        call.resolve()
+    }
+    
+    @objc func stopCurrentAndStartNewSession(_ call: CAPPluginCall) {
+        CAMDOReporter.stopCurrentAndStartNewSession()
+        call.resolve()
+    }
+    
+    @objc func startApplicationTransaction(_ call: CAPPluginCall) {
+        guard let transactionName = call.getString("transactionName"), !transactionName.isEmpty else {
+            call.resolve([
+                "error": "No transaction name"
+            ])
+            return
+        }
+        
+        let serviceName = call.getString("serviceName")
+        if serviceName != nil {
+            CAMDOReporter.startApplicationTransaction(withName: transactionName, service: serviceName) { completed, error in
+                
+                var errorStr: String? = nil
+                if error != nil {
+                    errorStr = self.CAMAAErrorString(error: error! as NSError)
+                }
+                call.resolve([
+                    "completed": completed,
+                    "error": errorStr as Any
+                ])
+            }
+        } else {
+            CAMDOReporter.startApplicationTransaction(withName: transactionName) { completed, error in
+                var errorStr: String? = nil
+                if error != nil {
+                    errorStr = self.CAMAAErrorString(error: error! as NSError)
+                }
+                call.resolve([
+                    "completed": completed,
+                    "error": errorStr as Any
+                ])
+            }
+        }
+    }
+    
+    @objc func stopApplicationTransaction(_ call: CAPPluginCall) {
+        guard let transactionName = call.getString("transactionName"), !transactionName.isEmpty else {
+            call.resolve([
+                "error": "No transaction name"
+            ])
+            return
+        }
+        
+        let failure = call.getString("failure")
+        if failure != nil {
+            CAMDOReporter.stopApplicationTransaction(withName: transactionName, failure: failure) { completed, error in
+                var errorStr: String? = nil
+                if error != nil {
+                    errorStr = self.CAMAAErrorString(error: error! as NSError)
+                }
+                call.resolve([
+                    "completed": completed,
+                    "error": errorStr as Any
+                ])
+            }
+        } else {
+            CAMDOReporter.stopApplicationTransaction(withName: transactionName) { completed, error in
+                var errorStr: String? = nil
+                if error != nil {
+                    errorStr = self.CAMAAErrorString(error: error! as NSError)
+                }
+                call.resolve([
+                    "completed": completed,
+                    "error": errorStr as Any
+                ])
+            }
+        }
+    }
+    
+    @objc func setCustomerFeedback(_ call: CAPPluginCall) {
+        guard let feedback = call.getString("feedback"), !feedback.isEmpty else {
+            call.reject("No feedback")
+            return
+        }
+        CAMDOReporter.setCustomerFeedback(feedback)
+        call.resolve()
+    }
+    
+    @objc func setCustomerLocation(_ call: CAPPluginCall) {
+        guard let postalCode = call.getString("postalCode"), let countryCode = call.getString("countryCode") else {
+            call.reject("No Postal code and country code")
+            return
+        }
+        CAMDOReporter.setCustomerLocation(postalCode, andCountry: countryCode)
+        call.resolve()
+    }
+    
+    @objc func sendScreenShot(_ call: CAPPluginCall) {
+        guard let name = call.getString("name"), !name.isEmpty else {
+            call.resolve([
+                "error": "No Screen name"
+            ])
+            return
+        }
+        
+        let quality = call.getString("quality")
+        var imageQuality: CGFloat = CAMAA_SCREENSHOT_QUALITY_DEFAULT
+        switch quality {
+        case "CAMAA_SCREENSHOT_QUALITY_DEFAULT":
+            imageQuality = CAMAA_SCREENSHOT_QUALITY_DEFAULT
+        case "CAMAA_SCREENSHOT_QUALITY_HIGH":
+            imageQuality = CAMAA_SCREENSHOT_QUALITY_HIGH
+        case "CAMAA_SCREENSHOT_QUALITY_MEDIUM":
+            imageQuality = CAMAA_SCREENSHOT_QUALITY_MEDIUM
+        case "CAMAA_SCREENSHOT_QUALITY_LOW":
+            imageQuality = CAMAA_SCREENSHOT_QUALITY_LOW
+        default:
+            imageQuality = CAMAA_SCREENSHOT_QUALITY_DEFAULT
+        }
+        
+        CAMDOReporter.sendScreenShot(name, withQuality: imageQuality) { completed, error in
+            var errorStr: String? = nil
+            if error != nil {
+                errorStr = self.CAMAAErrorString(error: error! as NSError)
+            }
+            call.resolve([
+                "completed": completed,
+                "error": errorStr as Any
+            ])
+        }
+    }
+    
+    @objc func viewLoaded(_ call: CAPPluginCall) {
+        guard let viewName = call.getString("viewName"), let loadTime = call.getFloat("loadTime") else {
+            call.reject("No viewName or loadTime")
+            return
+        }
+        CAMDOReporter.viewLoaded(viewName, loadTime: CGFloat(loadTime)) { completed, error in
+            var errorStr: String? = nil
+            if error != nil {
+                errorStr = self.CAMAAErrorString(error: error! as NSError)
+            }
+            call.resolve([
+                "completed": completed,
+                "error": errorStr as Any
+            ])
+        }
+    }
+    
+    @objc func ignoreView(_ call: CAPPluginCall) {
+        guard let viewName = call.getString("viewName"), !viewName.isEmpty else {
+            call.reject("No viewName")
+            return
+        }
+        CAMDOReporter.ignoreView(viewName)
+        call.resolve()
+    }
+    
+    @objc func ignoreViews(_ call: CAPPluginCall) {
+        guard let viewNames = call.getArray("viewNames", String.self), !viewNames.isEmpty else {
+            call.reject("No viewNames")
+            return
+        }
+        
+        CAMDOReporter.ignoreViews(Set(viewNames))
+        call.resolve()
+    }
+    
+    @objc func isScreenshotPolicyEnabled(_ call: CAPPluginCall) {
+        let isEnabled = CAMDOReporter.isScreenshotPolicyEnabled()
+        call.resolve([
+            "value": isEnabled
+        ])
+    }
+    
+    @objc func logNetworkEvent(_ call: CAPPluginCall) {
+        guard let url = call.getString("url"), let status = call.getInt("status"), let responseTime = call.getInt("responseTime"), let inBytes = call.getInt("inBytes"), let outBytes = call.getInt("outBytes") else {
+            call.reject("No url or status or responseTime or inBytes or outBytes")
+            return
+        }
+        CAMDOReporter.logNetworkEvent(url, withStatus: status, withResponseTime: Int64(responseTime), withInBytes: Int64(inBytes), withOutBytes: Int64(outBytes)) { completed, error in
+            var errorStr: String? = nil
+            if error != nil {
+                errorStr = self.CAMAAErrorString(error: error! as NSError)
+            }
+            call.resolve([
+                "completed": completed,
+                "error": errorStr as Any
+            ])
+        }
+    }
+    
+    @objc func logTextMetric(_ call: CAPPluginCall) {
+        guard let textMetricName = call.getString("textMetricName"), !textMetricName.isEmpty, let value = call.getString("value"), !value.isEmpty else {
+            call.resolve([
+                "error": "No Text Metric name or value"
+            ])
+            return
+        }
+        let attributes = call.getObject("attributes")
+        CAMDOReporter.logTextMetric(textMetricName, withValue: value, withAttributes: attributes as? NSMutableDictionary) { completed, error in
+            
+            var errorStr: String? = nil
+            if error != nil {
+                errorStr = self.CAMAAErrorString(error: error! as NSError)
+            }
+            call.resolve([
+                "completed": completed,
+                "error": errorStr as Any
+            ])
+        }
+    }
+    
+    @objc func logNumericMetric(_ call: CAPPluginCall) {
+        guard let numericMetricName = call.getString("numericMetricName"), !numericMetricName.isEmpty, let value = call.getDouble("value") else {
+            call.resolve([
+                "error": "No Numeric Metric name or value"
+            ])
+            return
+        }
+        let attributes = call.getObject("attributes")
+        CAMDOReporter.logNumericMetric(numericMetricName, withValue: value, withAttributes: attributes as? NSMutableDictionary) { completed, error in
+            var errorStr: String? = nil
+            if error != nil {
+                errorStr = self.CAMAAErrorString(error: error! as NSError)
+            }
+            call.resolve([
+                "completed": completed,
+                "error": errorStr as Any
+            ])
+        }
+    }
+    
+    @objc func uploadEvents(_ call: CAPPluginCall) {
+        DispatchQueue.main.async {
+            CAMDOReporter.uploadEvents { response, error in
+                var errorStr: String? = nil
+                if error != nil {
+                    errorStr = self.CAMAAErrorString(error: error! as NSError)
+                }
+                call.resolve([
+                    "response": response!,
+                    "error": errorStr as Any
+                ])
+            }
+        }
+    }
 }
 
